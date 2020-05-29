19900d064f25ecbbdb9edee168df7916b33c8bbf
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-05-29 20:03:30 +0300

    Goals App

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text> {outputText} </Text>
-      <Button title = "Change Text" onPress = {() => setOutputText('The text changed!')} />
+    
+    /*<View style={{                    //flex learning thingy
+      padding: 50, 
+      flexDirection: 'row', 
+      width: '80%', 
+      height: 300, 
+      justifyContent:'space-around', 
+      alignItems:'stretch'}}>
+
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+
+        <Text>1</Text>
+    
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+
+        <Text>2</Text>
+
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'green',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+
+        <Text>3</Text>
+
+      </View>
+
+    </View>
+  */
+
+    <View style = {styles.screen}>
+      <View style = {styles.inputContainer}>
+
+        <TextInput 
+          placeholder = "Course Goal" 
+          style = {styles.input}
+        />
+        
+        <Button title = "Add" />
+
+      </View>
+
+      <View>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  screen: {
+    padding:50
   },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'center'
+  },
+
+  input:{
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10 
+  }
 });
