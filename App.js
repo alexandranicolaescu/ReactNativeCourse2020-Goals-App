9290a8810edb9b6c7d85d903f2960d04877bd041
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-05-31 19:08:46 +0300

    Course Goal App (Listing Items + keys)

--- App.js
+++ App.js
@@ -1,10 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [ enteredGoal, setEnteredGoal ] = useState('');
+
+  const [courseGoals, setCourseGoals] = useState([]);
+  
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...courseGoals, 
+      {id: Math.random().toString(), value: enteredGoal }
+    ]);
+  };
+
+
   return (
-    
-    /*<View style={{                    //flex learning thingy
+
+    <View style = {styles.screen}>
+      <View style = {styles.inputContainer}>
+
+        <TextInput 
+          placeholder = "Course Goal" 
+          style = {styles.input}
+          onChangeText = {goalInputHandler}
+          value = {enteredGoal}
+        />
+        
+        <Button 
+          title = "Add" 
+          onPress = {addGoalHandler}
+        />
+
+      </View>
+
+      <FlatList 
+        keyExtractor = {(item, index) => item.id}
+        data = {courseGoals} 
+        renderItem = {itemData =>(
+          <View style ={styles.listItem}> 
+            <Text> {itemData.item.value} </Text> 
+          </View>
+        )}
+      />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  screen: {
+    padding:50
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'center'
+  },
+
+  input:{
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10 
+  },
+
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+
+
+
+
+
+  /*<View style={{                    //flex learning thingy
       padding: 50, 
       flexDirection: 'row', 
       width: '80%', 
@@ -53,43 +131,4 @@ export default function App() {
 
     </View>
   */
-
-    <View style = {styles.screen}>
-      <View style = {styles.inputContainer}>
-
-        <TextInput 
-          placeholder = "Course Goal" 
-          style = {styles.input}
-        />
-        
-        <Button title = "Add" />
-
-      </View>
-
-      <View>
-
-      </View>
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-
-  screen: {
-    padding:50
-  },
-
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems:'center'
-  },
-
-  input:{
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10 
-  }
 });
